ce2a84c24647363e943a9e68504dc5d6ccc5be55
ryanyunus <ryandinus@gmail.com>
2023-05-30 09:12:37 +0700

    binary 1

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+using namespace std;
+//node
+struct Node{
+    char label;
+    Node *left, *right, *parent;
+};
+
+//variabel pointer global 
+Node *root, *newNode;
+
+
+void createNewTree(char label)
+{
+    if(root != NULL)
+    {
+        cout <<"Tree sudah dibuat" <<endl;
+    }else{
+        root = new Node();
+        root->label = label;
+        root->left = NULL;
+        root->right = NULL;
+        root->parent = NULL;
+    }
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
