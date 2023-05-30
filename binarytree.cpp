36a01e1b2c89c3490fd4e3dda555f6b1d635ebb5
ryanyunus <ryandinus@gmail.com>
2023-05-30 10:03:33 +0700

    bintree 3

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 using namespace std;
-//node
-struct Node{
+
+// Node structure
+struct Node {
     char label;
-    Node *left, *right, *parent;
+    Node* left;
+    Node* right;
+    Node* parent;
 };
 
-//variabel pointer global 
-Node *root, *newNode;
+// Global pointer variables
+Node* root;
+Node* newNode;
 
 
-void createNewTree(char label)
-{
-    if(root != NULL)
-    {
-        cout <<"Tree sudah dibuat" <<endl;
-    }else{
+void createNewTree(char label) {
+    if (root != NULL) {
+        cout << "Tree sudah dibuat" << endl;
+    }
+    else {
         root = new Node();
         root->label = label;
         root->left = NULL;
         root->right = NULL;
         root->parent = NULL;
-        cout << "\n" << "berhasil dibuat menjadi root." << endl;
+        cout << "Berhasil dibuat menjadi root." << endl;
+    }
+}
+
+Node* insertLeft(char label, Node* node) {
+    if (root == NULL) {
+        cout << "Buat tree terlebih dahulu" << endl;
+        return NULL;
+    }
+    else {
+        newNode = new Node();
+        newNode->label = label;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->parent = node;
+        node->left = newNode;
+        cout << label << " berhasil ditambahkan ke anak kiri. Label parent: " << newNode->parent->label << endl;
+        return newNode;
     }
 }
 
 int main() {
-  createNewTree('A');
-}
+    createNewTree('A');
+    Node* nodeB, * nodeC, * nodeD, * nodeE, * nodeF, * nodeG, * nodeH, * nodeI, * nodeJ;
+    nodeB = insertLeft('B', root);
+    cout << root->left->label <<endl;
 
+    return 0;
+}
